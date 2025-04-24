539149d022bb45b8b7f5c04182a7f2e0bff05baa
Taidgh Robinson <taidgh.robinson@maine.edu>
2025-04-24 16:49:45 -0400

    Adding a couple more things, still a rough POC

--- nba.py
+++ nba.py
@@ -40,10 +40,16 @@ def get_final_score(game: Any) -> dict:
     team_2_pts = int(team_2['PTS'].sum())
     return {team_1_name: team_1_pts, team_2_name: team_2_pts}
 
+def filter_to_pra_columns(game: Any) -> Any:
+    return game[['PLAYER_NAME', 'TEAM_CITY', 'PTS', 'REB', 'AST']]
+
+def filter_to_full_columns(game: Any) -> Any:
+    return game[['PLAYER_NAME', 'TEAM_CITY', 'PTS', 'REB', 'AST', 'STL', 'BLK', 'PLUS_MINUS', 'TO', 'PF', 'MIN']]
+
+
 @mcp.tool()
 async def get_game_ids_tool() -> str:
     """Get the game IDs for all the games that happened yesterday."""
-
     return get_game_ids()
 
 @mcp.tool()
@@ -56,7 +62,9 @@ async def get_game_score() -> str:
 
 @mcp.tool()
 async def get_game_scores() -> list:
-    """Get the score for all games that happened yesterady. It should be of the format Team 1: Score 1 - Team 2: Score 2. It would be great if claude to take the team name and return the full name, for example if dict 1 item is Memphis it would be great if it could return Memphis Grizzlies"""
+    """Get the score for all games that happened yesterday. 
+    It should be of the format Team 1: Score 1 - Team 2: Score 2. 
+    It would be great if claude to take the team name and return the full name, for example if dict 1 item is Memphis it would be great if it could return Memphis Grizzlies"""
     scores = []
     for gid in get_game_ids():
         d = get_final_score(get_game_box_score(gid))
@@ -84,10 +92,35 @@ async def get_four_factors(game_filter=None, table_view=False) -> dict:
 
     return ffs
 
+@mcp.tool()
+async def get_pra_breakdown(game_filter=None, table_view=False):
+    """Get the points rebounds and assists for all players that played in all games that happened yesterday. 
+    It should start with a bolded title of the two teams that played, for example Memphis Grizzles - Los Angles Lakers and then list the four factors underneath. 
+    It can take an optional game title, for example 'Memphis Grizzlies game' or 'lakers game', in which case it should only return the four factors for that game.'
+    It can take the option to display the data in a table view as well, if it is a table view it should be two tables, one for each team."""
+    games = []
+    for gid in get_game_ids():
+        games.append(filter_to_pra_columns(get_game_box_score(gid)))
+
+    return games
+
+@mcp.tool()
+async def get_full_breakdown(game_filter=None, table_view=False):
+    """Get the points rebounds, assists steals, blocks, plus minus, turn overs, personal fouls, and minutes played for all players that played in all games that happened yesterday. 
+    It should start with a bolded title of the two teams that played, for example Memphis Grizzles - Los Angles Lakers and then list the four factors underneath. 
+    It can take an optional game title, for example 'Memphis Grizzlies game' or 'lakers game', in which case it should only return the four factors for that game.'
+    It can take the option to display the data in a table view as well, if it is a table view it should be two tables, one for each team."""
+    games = {}
+    for gid in get_game_ids():
+        game = filter_to_full_columns(get_game_box_score(gid))
+        teams = set (game['TEAM_CITY'] )
+        team_1_name = teams.pop()
+        team_2_name = teams.pop()
+        games[str(team_1_name)+'-'+str(team_2_name)] = game
+
+    return games
+
     
 if __name__ == "__main__":
-
-    for gid in get_game_ids():
-        print(get_final_score(get_game_box_score(gid)))
     # Initialize and run the server
     mcp.run(transport='stdio')
